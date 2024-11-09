c35a775e99562c272c6f1ff519312dfa6cb035ee
jacqueline294 <144172392+jacqueline294@users.noreply.github.com>
2024-11-09 04:42:18 +0100

    adding the circluar profile image  and refactor the app

---
+++ MessagerAppProject/Core/Components/CircularProfileImageView.swift
@@ -0,0 +1,57 @@
+//
+//  CircularProfileImageView.swift
+//  MessagerAppProject
+//
+//  Created by jacqueline Ngigi on 2024-11-08.
+//
+
+import SwiftUI
+
+enum ProfileImageSize{
+    case xxSmall
+    case xSmall
+    case small
+    case medium
+    case large
+    case xlarge
+    
+    var dimension: CGFloat{
+        switch self {
+        case .xxSmall: return 28
+        case .xSmall: return 32
+        case .small: return 40
+        case .medium: return 56
+        case .large: return 64
+        case .xlarge: return 80
+        }
+    }
+    
+}
+
+struct CircularProfileImageView: View {
+    let user: User
+    let size: ProfileImageSize
+    
+    var body: some View{
+        if  let imageUrl = user.profileImageUrl {
+            Image(imageUrl)
+                .resizable()
+                .scaledToFill()
+                .frame(width: size.dimension, height: size.dimension)
+                .clipShape(Circle())
+        }else {
+            Image(systemName: "")
+                .resizable()
+                .frame(width:size.dimension, height: size.dimension)
+                .foregroundColor(Color(.systemGray4))
+               
+            
+        }
+        }
+    }
+
+
+
+#Preview {
+    CircularProfileImageView(user: User.MOCK_USER,size: .medium)
+}
